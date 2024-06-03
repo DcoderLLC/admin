89fc86999d656d10ad0aa0ea58b4e45f1b07a270
DcoderLLC <dcoderllc@gmail.com>
2024-06-03 07:34:40 +0530

    Data Setup completed

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import {} from "react";
+import "./App.css";
+import {Box} from "@mui/material"
+
+function App() {
+  return (
+    <Box>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Box>
+  );
+}
+
+export default App;
